b57a937607c73958cd0390d656e83b3330d068b7
Nils <nmittel@protonmail.com>
2022-11-23 14:09:01 +0100

    Ajout de la page de jeu et ajout d'une Map

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,103 @@
 const elements = [];
-
 var video;
 var startGameButton;
 var signInButton;
+var gamePage;
+
+
+
+
+class City{
+    constructor(name, lat, long){
+        this.name =name;
+        this.lat = lat;
+        this.long = long;
+    }
+
+}
+
+var city = new City("", 0, 0);
 
 function main(){
     video = document.getElementById('video');
     elements.push(video);
+    gamePage = document.getElementById('game');
+    elements.push(gamePage);
     video.playbackRate = 0.9;
-
-    startGameButton = document.getElementById('startGame');
-    elements.push(startGameButton);
-    startGameButton.addEventListener("click", startGame);
-
 }
 
 
 function startGame(){
-    generateCoordinates();
+    getRandomCity();
     loadGamePage();
 }
 
-function generateCoordinates(){
+function getRandomCity(){
+    getNumberOfCities(); //rename
+
     
+    setTimeout(null,2000);
+
 }
+
+function getNumberOfCities(){
+
+    var request = new XMLHttpRequest();
+
+    var url = 'http://geodb-free-service.wirefreethought.com/v1/geo/cities?hateoasMode=off';
+
+    request.open("GET", url, true);
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var parsedResponse = JSON.parse(this.responseText); 
+            console.log(parsedResponse);
+            nbCities = parsedResponse.metadata.totalCount;
+            generateCityNumber(nbCities) ;
+        }
+    }
+    request.send();
+}
+
+function generateCityNumber(){
+    var cityNumber= Math.floor(Math.random() * nbCities);
+    getCity(cityNumber);
+}
+
+function getCity(cityNumber){
+    var request = new XMLHttpRequest();
+
+    var url = 'http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=' + cityNumber + '&hateoasMode=off';
+
+    request.open("GET", url, true);
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var parsedResponse = JSON.parse(this.responseText); 
+            console.log(parsedResponse);
+            console.log(parsedResponse.data[0].name);
+            city = new City(parsedResponse.data[0].name, parsedResponse.data[0].latitude, parsedResponse.data[0].longetitude);
+            var cityName = document.getElementById('cityName');
+            cityName.textContent= (city.name);
+        }
+    }   
+
+    request.send();
+}
+
 function loadGamePage(){
-    hideElements();
+    transition();
+    var map = L.map('map').setView([51.505, -0.09], 2);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+}
+
+function transition(){
+
 }
 
 function hideElements(){
