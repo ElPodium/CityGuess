e9f480b63a64755e41f928c498a123507dce84b8
akash47 <65364908+Akashos23@users.noreply.github.com>
2022-12-03 18:41:11 +0100

    update js

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,50 @@
-const elements = [];
-
-var video;
-var startGameButton;
-var signInButton;
 var confirmerButton;
 var map;
-var cercle;
 var latlngfind;
-var cityfind;
+//var cityfind;
+var moinButton;
+var pleinecran;
+var tour;
+var score;
+var distances;
+var markers = new Array();
 
 function main(){
-    /*
-    video = document.getElementById('video');
-    elements.push(video);*/
-    //video.playbackRate = 0.9;
     map = L.map('map').setView([51.505, -0.09], 7);
-    //cercle = L.circle();
-
-    /*startGameButton = document.getElementById('startGame');
-    elements.push(startGameButton);
-    startGameButton.addEventListener("click", startGame);*/
-    startGame();
-
-}
-
-function startGame(){
-    generateCoordinates();
-    loadGamePage();
-}
-
-function generateCoordinates(){
-}    
-
-function loadGamePage(){
-    hideElements();
-}
-
-function hideElements(){
+    moinButton = document.getElementById("minus");
+    pleinecran = document.getElementById("pleinecran");
+    pleinecran.addEventListener("click", PleinEcran);
+    moinButton.addEventListener("click", PageMoin);
+    score = 0;
+    tour = 1;
     mapleeflet();
+}
+
+function PleinEcran(){
+
+
+}
+
+function PageMoin(){
+    var divsToHide = document.getElementsByClassName("hide"); //divsToHide is an array
+    for(var i = 0; i < divsToHide.length; i++){
+        divsToHide[i].style.visibility = "hidden"; // or
+        divsToHide[i].style.display = "none"; // depending on what you're doing
+    }
+    moinButton.innerHTML = "+";
+    moinButton.removeEventListener("click", PageMoin);
+    moinButton.addEventListener("click", PageGrand);
+}
+
+function PageGrand(){
+    var divsToHide = document.getElementsByClassName("hide"); //divsToHide is an array
+    for(var i = 0; i < divsToHide.length; i++){
+        divsToHide[i].style.visibility = "visible"; // or
+        divsToHide[i].style.display = "inline-block"; // depending on what you're doing
+    }
+    moinButton.innerHTML = "-";
+    moinButton.removeEventListener("click", PageGrand);
+    moinButton.addEventListener("click", PageMoin);
 }
 
 function mapleeflet(){
@@ -51,23 +58,28 @@ function mapleeflet(){
 function onMapClick(e) {
     confirmerButton = document.getElementById('confirmer');
     var marker = L.marker([e.latlng.lat, e.latlng.lng]);
+    markers.push(marker);
     marker.addTo(map);
-      latlngfind = e.latlng;
+    latlngfind = e.latlng;
+    if(markers.length > 1){
+        map.removeLayer(markers[0]);
+        markers.splice(0, 1);
+    } 
     confirmerButton.addEventListener("click", ville);
-    
 }
 
 function ville(){
     map.removeEventListener("click", onMapClick);
-    var url = "https://us1.locationiq.com/v1/reverse?key=pk.938afe8c6000b6dd94d2262604cb7ebb&lat="+latlngfind.lat+"&lon="+latlngfind.lng+"&format=json"
-    /*var data = {
+    /*var url = "https://us1.locationiq.com/v1/reverse"
+    var datas = {
         key : "pk.938afe8c6000b6dd94d2262604cb7ebb",
         lat : latlngfind.lat,
         lon : latlngfind.lng,
         format : "json"
-    };*/
+    };
     $.ajax({
         url: url,
+        data : datas,
         method:"GET",
         dataType:"JSON",
         success: function(retour){
@@ -76,20 +88,35 @@ function ville(){
          error : function() {
              alert("PB avec l'URL");
          }	
-    });
-    var url2 = "https://us1.locationiq.com/v1/search?key=pk.938afe8c6000b6dd94d2262604cb7ebb&q=221b%2C%20Baker%20St%2C%20London%20&format=json"
+    });*/
+    VilleAtrouver();
+}
+
+function VilleAtrouver(){
+    confirmerButton.removeEventListener("click", ville);
+    var url2 = "https://us1.locationiq.com/v1/search"
+    var datas = {
+        key : "pk.938afe8c6000b6dd94d2262604cb7ebb",
+        q : "221b, Baker St, London ",
+        format : "json",
+    };
     $.ajax({
         url: url2,
+        data: datas,
         method:"GET",
         dataType:"JSON",
         success: function(retour){
-            var markers = L.marker([retour[0].lat, retour[0].lon]);
-            markers.addTo(map);
+            var marker = L.marker([retour[0].lat, retour[0].lon]);
+            marker.addTo(map);
             var latlngs = [
                 [retour[0].lat, retour[0].lon],
                 [latlngfind.lat, latlngfind.lng]
             ];
             var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+            markers.push(marker);
+            markers.push(polyline);
+            var markerArrivé = marker.getLatLng();
+            DistanceVille(latlngfind, markerArrivé);
          },
          error : function() {
              alert("PB avec l'URL");
@@ -97,14 +124,44 @@ function ville(){
     });
 }
 
+function DistanceVille(from, to)
+{
+    var distanceKm=from.distanceTo(to).toFixed(0)/1000;
+    document.getElementById("distance").innerText = "Distance : " + distanceKm + " km";
+    resultat = 100000 - (distanceKm*10);
+    if(resultat < 0){
+        resultat = 0;
+    }
+    score += Math.round(resultat);
+    document.getElementById("Scorer").innerText = "Score : " + score;
+    confirmerButton.innerText = "Prochain"
+    confirmerButton.addEventListener("click", ProchainTour);
+}
+
+function ProchainTour(){
+    tour += 1;
+    document.getElementById("tour").innerText = "Tour n°" + tour;
+    document.getElementById("distance").innerText = "Distance : 0 km";
+    for(var i = 0; i < markers.length; i++){
+        map.removeLayer(markers[i]);
+        markers.splice(i, 1);
+    }
+    map.removeLayer(markers[0]);
+    markers.splice(0, 1);
+    confirmerButton.innerText = "Confirmer";
+    Suivant();
+}
+
+function Suivant(){
+    confirmerButton.removeEventListener("click", ProchainTour);
+    mapleeflet();
+}
+
 function streetviewgenerator(){
     $.ajax({
         url : "https://random-ize.com/random-map/map-f.php", 
         method : "GET",
         dataType : "JSON",
-        header: {
-            'Access-Control-Allow-Origin': '*',
-        },
         beforeSend: function(xhr){
             xhr.setRequestHeader ("Authorization", "Basic" + btoa(""));
         },
@@ -116,7 +173,6 @@ function streetviewgenerator(){
         }	
     });
 }
-
 
 
 
