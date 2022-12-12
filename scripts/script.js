63e360d8d5b8b6044064cca9b220faac0d4b3ff2
akash47 <65364908+Akashos23@users.noreply.github.com>
2022-12-12 12:42:37 +0100

    update js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ var map;
 var latlngfind;
 var cityfind;
 var moinButton;
-var pleinecran;
 var easyButton;
 var tour;
 var score;
@@ -14,70 +13,12 @@ function main(){
     streetviewgenerator();
     map = L.map('map').setView([51.505, -0.09], 7);
     moinButton = document.getElementById("minus");
-    //pleinecran = document.getElementById("pleinecran");
-    //pleinecran.addEventListener("click", PleinEcran);
     moinButton.addEventListener("click", PageMoin);
     score = 0;
     tour = 1;
     mapleeflet();
 }
 
-/*function PleinEcran(){
-document.getElementById("Leaflets").innerHTML =  `<div id = "Leaflets" class="flex items-start justify-end">
-<div id = "BlockLeaf" class="hide absolute border-2 border-#BBCBC4-400 bg-white bg-opacity-75 z-2 px-4 py-2 w-screen h-screen rounded">
-    <p id = "tour"class="text-center text-xl py-2 px-1">Tour n°`+tour+`</p>
-    <p id="Scorer" class="text-start text-xl py-2 px-1">Score : `+score+`</p>
-    <p id="distance" class="distance text-start text-xl py-1 px-1">Distance : 0</p>
-<div class="flex justify-end py-2">
-    <button type="button" id="pleinecran" class="cursor-pointer rounded-lg border-2 border-white px-4 py-2.5 leading-tight rounded hover:bg-black hover:bg-opacity-10 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out"><img class="w-4" src="./assets/P-ecran.jpg"></button>
-</div>
-<div id="map" class="h-2/3 w-auto">
-
-</div>
-<div class="flex justify-center py-2">
-    <button type="button" id="confirmer" class="cursor-pointer rounded-lg border-2 border-white  px-8 py-2.5 text-black font-medium text-lg leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">Confirmer</button>
-</div>
-</div>
-</div>`
-map = L.map('map').setView([51.505, -0.09], 7);
-document.getElementById("pleinecran").removeEventListener("click", PleinEcran);
-document.getElementById("pleinecran").addEventListener("click", miniEcran);
-if(markers.length == 0){
-    mapleeflet();
-}
-else{
-    mapRecover();
-}
-}
-
-function miniEcran(){
-document.getElementById("Leaflets").innerHTML = `<div id = "Leaflets" class="flex items-start justify-end">
-<button type="button" id="minus" class="absolute cursor-pointer rounded-full border-2 border-white px-3 py-1 text-black font-medium text-lg leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">-</button>
-<div id = "BlockLeaf"class="hide absolute border-2 border-#BBCBC4-400 bg-white bg-opacity-75 mt-8 z-2 px-4 py-2 w-96 mr-4 rounded">
-    <p id = "tour"class="text-center text-m py-2 px-1">Tour n°`+tour+`</p>
-    <p id="Scorer" class="text-start text-m py-2 px-1">Score : `+score+`</p>
-    <p id="distance" class="distance text-start text-m py-1 px-1">Distance : 0</p>
-<div class="flex justify-end py-2">
-    <button type="button" id="pleinecran" class="cursor-pointer rounded-lg border-2 border-white px-4 py-2.5 text-black font-medium text-lg leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out"><img class="w-4" src="./assets/P-ecran.jpg"></button>
-</div>
-<div id="map" class="h-80 w-80">
-
-</div>
-<div class="flex justify-center py-2">
-    <button type="button" id="confirmer" class="cursor-pointer rounded-lg border-2 border-white  px-8 py-2.5 text-black font-medium text-lg leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">Confirmer</button>
-</div>
-</div>
-</div>`
-map = L.map('map').setView([51.505, -0.09], 7);
-document.getElementById("pleinecran").removeEventListener("click", miniEcran);
-document.getElementById("pleinecran").addEventListener("click", PleinEcran);
-if(markers.length == 0){
-    mapleeflet();
-}
-else{
-    mapRecover();
-}
-}*/
 
 function PageMoin(){
     var divsToHide = document.getElementsByClassName("hide"); //divsToHide is an array
@@ -109,30 +50,6 @@ function mapleeflet(){
     }).addTo(map);
     map.addEventListener("click", onMapClick);
 }
-
-/*function mapRecover(){
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    noWrap: true, 
-    maxZoom: 30,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    L.marker([markers[0]._latlng.lat, markers[0]._latlng.lng]).addTo(map);
-    if(markers.length > 1) {
-        L.marker([markers[1]._latlng.lat, markers[1]._latlng.lng]).addTo(map);
-        var latlngs = [
-            [markers[0]._latlng.lat, markers[0]._latlng.lng],
-            [markers[1]._latlng.lat, markers[1]._latlng.lng]
-        ];
-        L.polyline(latlngs, {color: 'red'}).addTo(map);
-        confirmerButton = document.getElementById('confirmer');
-        confirmerButton.innerText = "Prochain";
-        confirmerButton.addEventListener("click", ProchainTour);
-    }
-    else{
-        map.addEventListener("click", onMapClick);
-    }
-    
-}*/
 
 function onMapClick(e) {
     confirmerButton = document.getElementById('confirmer');
